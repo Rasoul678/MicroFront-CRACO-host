d350490f65d2d9e90dc38334bc7d21b1712e5937
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-30 10:31:13 +0330

    serve on build

--- src/Host.jsx
+++ src/Host.jsx
@@ -1,11 +1,14 @@
 import Remote from "remote/Remote";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Host = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is Host</h1>
-        <Remote isRemote />
+        <ErrorBoundary>
+          <Remote isRemote />
+        </ErrorBoundary>
       </header>
     </div>
   );
